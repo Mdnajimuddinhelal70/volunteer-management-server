a495e38c0a9790eb799853bb9c2abe5d8e8599f8
najim77288 <najimuddin77288@gmail.com>
2024-08-12 22:31:40 +0600

    aded funtionality for getting data by email

--- index.js
+++ index.js
@@ -21,59 +21,80 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const volunteerCollection = client.db("volunteerDb").collection("volunteer");
-    const formVolunteerCollection = client.db("volunteerDb").collection("formVolunteer");
+    const volunteerCollection = client
+      .db("volunteerDb")
+      .collection("volunteer");
+    const formVolunteerCollection = client
+      .db("volunteerDb")
+      .collection("formVolunteer");
 
-    app.get('/volunteer', async (req, res) => {
-        const result = await volunteerCollection.find().sort({ deadline: 1 }) 
-          .limit(3) 
-          .toArray();
-        res.json(result);    
+    app.get("/volunteer", async (req, res) => {
+      const result = await volunteerCollection
+        .find()
+        .sort({ deadline: 1 })
+        .limit(3)
+        .toArray();
+      res.json(result);
     });
-          // API for showing all cards and filtering
-      app.get('/volunteerAll', async (req, res) => {
-        const filter = req.query;
-        const query = {
-          postTitle: {$regex: filter.search, $options: 'i'}
-        }
-          const result = await volunteerCollection.find(query).toArray();
-          res.json(result);   
+    // API for showing all cards and filtering
+    app.get("/volunteerAll", async (req, res) => {
+      const filter = req.query;
+      const query = {
+        postTitle: { $regex: filter.search, $options: "i" },
+      };
+      const result = await volunteerCollection.find(query).toArray();
+      res.json(result);
+    });
+
+    app.get("/volunteerDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await volunteerCollection.findOne({
+        _id: new ObjectId(id),
       });
-
-
-    app.get("/volunteerDetails/:id", async (req, res) => {  
-        const id = req.params.id;
-        const result = await volunteerCollection.findOne({
-          _id: new ObjectId(id),
-        });
-        res.send(result);
+      res.send(result);
     });
 
-    //api for request send
-    app.post('/submit-volunteer-request/:email', async (req, res) => {
-      const email = req.params.email; 
-      const requestData = req.body; 
-      requestData.email = email; 
-      const result = await formVolunteerCollection.insertOne(requestData);
-      res.send(result); 
+
+    app.post("/submit-volunteer-request/:email", async (req, res) => {
+      const email = req.params.email;
+      const requestData = req.body;
+
+      try {
+        requestData.email = email;
+        const result = await formVolunteerCollection.insertOne(requestData);
+        res.status(200).send(result);
+      } catch (error) {
+        console.error("Error submitting volunteer request:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
     });
+
+    app.get("/volunteer-need-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await volunteerCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+    //
 
     app.post("/formVolunteer", async (req, res) => {
       const result = await volunteerCollection.insertOne(req.body);
       res.send(result);
     });
 
-    app.get("/volunteer-need-details/:id", async (req, res) => {
-      const id = req.params.id;
-      const result = await volunteerCollection.findOne({_id: new ObjectId(id)});
+    app.get("/needPost/:email", async (req, res) => {
+      const email = req.params.email
+      const result = await formVolunteerCollection.find({email: email}).toArray();
       res.send(result);
     });
 
-    app.get("/needPost", async (req, res) => {
-      const result = await formVolunteerCollection.find().toArray();
-      res.send(result);
-    });
-
+   app.delete('/deleteNeedPost/:id', async(req, res) => {
+    const id = req.params.id;
+    const result = formVolunteerCollection.deleteOne({_id: new ObjectId(id)});
+    res.send(result);
+   })
+      
     // API for deleting a volunteer
     app.delete("/deleteVolunteer/:id", async (req, res) => {
       const result = await formVolunteerCollection.deleteOne({
@@ -82,31 +103,35 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/volunteerPostUpdate/:id', async (req, res) => {
-      const result = await formVolunteerCollection.find().toArray();
+    app.get("/myNeedPost/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await formVolunteerCollection.find({ email: email }).toArray();
       res.send(result);
     });
 
-    app.get('/myNeedPost', async(req, res) => {
-      const result = await formVolunteerCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/volunteerPostUpdate/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await formVolunteerCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
 
     app.put("/updateVolunteerPost/:id", async (req, res) => {
-        const query = { _id: new ObjectId(req.params.id) };
-        const data = {
-          $set: {
-            thumbnail: req.body.thumbnail,
-            postTitle: req.body.postTitle,
-            description: req.body.description,
-            category: req.body.category,
-            location: req.body.location,
-            noOfVolunteersNeeded: req.body.noOfVolunteersNeeded,
-            deadline: req.body.deadline,
-          },
-        };
-        const result = await formVolunteerCollection.updateOne(query, data);
-        res.send(result); 
+      const query = { _id: new ObjectId(req.params.id) };
+      const data = {
+        $set: {
+          thumbnail: req.body.thumbnail,
+          postTitle: req.body.postTitle,
+          description: req.body.description,
+          category: req.body.category,
+          location: req.body.location,
+          noOfVolunteersNeeded: req.body.noOfVolunteersNeeded,
+          deadline: req.body.deadline,
+        },
+      };
+      const result = await formVolunteerCollection.updateOne(query, data);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
